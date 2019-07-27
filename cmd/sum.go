bf77d763398062a0be7677f7e958df80946fe585
niboshi <niboshiporipori@gmail.com>
2019-07-27 10:32:52 +0900

    Add args validation (#7)

--- cmd/sum.go
+++ cmd/sum.go
@@ -1,17 +1,28 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/mpppk/cli-template/lib"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
+	"strconv"
 )
 
 var sumCmd = &cobra.Command{
 	Use:   "sum",
 	Short: "print sum of arguments",
 	Long: ``,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(lib.SumFromString(args))
+	Args: cobra.MinimumNArgs(2),
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		for _, arg := range args {
+			if _, err := strconv.Atoi(arg); err != nil {
+				return errors.Wrapf(err, "failed to convert args to int: ", arg)
+			}
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cmd.Println(lib.SumFromString(args))
+		return nil
 	},
 }
 
