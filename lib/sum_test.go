e4f753d774956af6447bc95b3fcd7ea4499fb08e
mpppk <niboshiporipori@gmail.com>
2019-07-28 12:09:51 +0900

    Add testdata

--- lib/sum_test.go
+++ lib/sum_test.go
@@ -1,6 +1,10 @@
 package lib
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 	"testing"
 )
 
@@ -28,6 +32,55 @@ func TestSum(t *testing.T) {
 			wantSum: 2,
 		},
 	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotSum := Sum(tt.args.numbers); gotSum != tt.wantSum {
+				t.Errorf("Sum() = %v, want %v", gotSum, tt.wantSum)
+			}
+		})
+	}
+}
+
+func TestSumFromFile(t *testing.T) {
+	type args struct {
+		numbers []int
+	}
+
+	type tCase struct {
+		name    string
+		args    args
+		wantSum int
+	}
+
+	f, err := os.Open("../testdata/sum.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	contentBytes, err := ioutil.ReadAll(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+	contents := string(contentBytes)
+	lines := strings.Split(contents, "\n")
+	var tests []tCase
+	for i, line := range lines {
+		strRow := strings.Split(line, " ")
+		row, err := ConvertStringSliceToIntSlice(strRow)
+		if err != nil {
+			t.Fatal(err)
+		}
+		want := row[len(row)-1]
+		nums := row[:len(row)-1]
+		tc := tCase{
+			name: fmt.Sprintf("case%d\n", i),
+			args: args{
+				numbers: nums,
+			},
+			wantSum: want,
+		}
+		tests = append(tests, tc)
+	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if gotSum := Sum(tt.args.numbers); gotSum != tt.wantSum {
