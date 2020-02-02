31fc90034a0ee07738d920e179119d1e3355d5ce
niboshi <niboshiporipori@gmail.com>
2020-02-02 23:28:27 +0900

    Add domain and usecase package (#37)
    * Add domain and usecase package
    
    * Add comments
    
    * Add test of domain and usecase
    
    * Fix comment

---
+++ pkg/usecase/sum.go
@@ -0,0 +1,24 @@
+package usecase
+
+import (
+	"github.com/mpppk/cli-template/pkg/domain"
+	"golang.org/x/xerrors"
+)
+
+// CalcSumFromStringSlice is use case to calculate sum from string slice
+func CalcSumFromStringSlice(strNumbers []string) (int, error) {
+	numbers, err := domain.NewNumbersFromStringSlice(strNumbers)
+	if err != nil {
+		return 0, xerrors.Errorf("failed to create numbers from string slice: %w", err)
+	}
+	return numbers.CalcSum(), nil
+}
+
+// CalcL1NormFromStringSlice is use case to calculate L1 norm from string slice
+func CalcL1NormFromStringSlice(strNumbers []string) (int, error) {
+	numbers, err := domain.NewNumbersFromStringSlice(strNumbers)
+	if err != nil {
+		return 0, xerrors.Errorf("failed to create numbers from string slice: %w", err)
+	}
+	return numbers.CalcL1Norm(), nil
+}
