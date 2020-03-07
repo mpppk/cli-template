02fed4b9295bb4bc65cc99b423098fe3bdf32867
mpppk <niboshiporipori@gmail.com>
2020-03-07 17:51:13 +0900

    Add more comment

--- repoimpl/memory_sum_history.go
+++ repoimpl/memory_sum_history.go
@@ -7,19 +7,23 @@ import (
 	"github.com/mpppk/cli-template/domain/repository"
 )
 
+// MemorySumHistory represents on memory repository of sum history
 type MemorySumHistory struct {
 	history []*model.SumHistory
 }
 
+// NewMemorySumHistory create new MemorySumHistory repository
 func NewMemorySumHistory() repository.SumHistory {
 	return &MemorySumHistory{history: []*model.SumHistory{}}
 }
 
+// Add add new sum history to repository
 func (m *MemorySumHistory) Add(sumHistory *model.SumHistory) {
 	m.history = append(m.history, sumHistory)
 	log.Printf("current history: %v", m.history)
 }
 
+// List return list of sum history
 func (m *MemorySumHistory) List(limit int) []*model.SumHistory {
 	l := len(m.history)
 	if l <= limit {
